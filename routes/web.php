f0a56e261020e3b73011ff0fb577eab9beeaa989
Dimitris <jvortelinas@gmail.com>
2025-02-27 11:18:34 +0100

    nav ok

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/vegetarian', function () {
+    return view('all_recipes');
+});
+Route::get('/', action: function () {
+    return view('vegetarian');
+});
+Route::get('/sweets', function () {
+    return view('sweets');
+});
+Route::get('/wines', function () {
+    return view('wines');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
