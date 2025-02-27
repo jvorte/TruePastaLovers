8b1dab843d857cc5f8cb8f374c8d2f4b4f7c52d8
Dimitris <jvortelinas@gmail.com>
2025-02-27 19:39:21 +0100

    routes ok auth ok

--- resources/views/all_recipes.blade.php
+++ resources/views/all_recipes.blade.php
@@ -4,7 +4,7 @@
         <p class="text-3xl md:text-4xl lg:text-5xl">All Recipes <span class="text-1xl md:text-2xl lg:text-3xl">from True Pasta Lovers</span> </p>
         @auth
             @if(Auth::user()->isAdmin())
-                <a href="{{ url('/') }}" class="text-md">+ new</a>
+                <a href="{{ url('/recipes/create') }}" class="text-md">+ new</a>
             @endif
         @endauth
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +26,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+
+
+Route::resource('recipes', RecipeController::class);
+
+    Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
